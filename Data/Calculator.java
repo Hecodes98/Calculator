4c5eb9e161359baf7d1956b94176337a7c505ee5
Hector_Cardona <hector.cardona@globant.com>
2021-01-21 13:18:34 -0500

    Sum & subtract methods added

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -1,5 +1,10 @@
 package Data;
 
 public class Calculator{
-    
+    public int sum(int number1, int number2){
+        return number1+number2;
+    }
+    public int subtract(int number1, int number2){
+        return number1-number2;
+    }
 }
