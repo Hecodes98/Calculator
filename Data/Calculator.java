20fdb4010e626e39ba4c9fe2937a62be61476d32
Hector_Cardona <hector.cardona@globant.com>
2021-01-21 13:31:04 -0500

    methods erased

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -1,10 +1,5 @@
 package Data;
 
 public class Calculator{
-    public int sum(int number1, int number2){
-        return number1+number2;
-    }
-    public int subtract(int number1, int number2){
-        return number1-number2;
-    }
+
 }
