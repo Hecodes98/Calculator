86be8d1069ed22433e9c2bd30860f5bbdb6e25eb
Hector_Cardona <hector.cardona@globant.com>
2021-01-21 22:02:33 -0500

    git rebase

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator{
     public int sum(int number1, int number2){
         return number1+number2;
     }
+    
     public int subtract(int number1, int number2){
         return number1-number2;
     public int multi(int number1, int number2){
