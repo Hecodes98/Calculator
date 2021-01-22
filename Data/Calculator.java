386d799fbd20e56ba1ab7f2003c35c2c4271da3b
Hector_Cardona <hector.cardona@globant.com>
2021-01-21 21:52:00 -0500

    multi & div methods added

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -6,5 +6,10 @@ public class Calculator{
     }
     public int subtract(int number1, int number2){
         return number1-number2;
+    public int multi(int number1, int number2){
+        return number1*number2;
+    }
+    public int div(int number1, int number2){
+        return number1/number2;
     }
 }
