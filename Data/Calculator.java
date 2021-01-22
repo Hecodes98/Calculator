fe81b2799fc4754101efb2f86b61da990d2351c6
Hector_Cardona <hector.cardona@globant.com>
2021-01-21 22:08:28 -0500

    fix conflict

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -1,15 +1,12 @@
 package Data;
 
 public class Calculator{
-<<<<<<< HEAD
     public int sum(int number1, int number2){
         return number1+number2;
     }
-    
     public int subtract(int number1, int number2){
         return number1-number2;
-=======
->>>>>>> 1e4ddd0106caecdc9ca55ba2fe306ff097f6d117
+    }
     public int multi(int number1, int number2){
         return number1*number2;
     }
