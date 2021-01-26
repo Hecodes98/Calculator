8edddc5e723e2996f0cced76c1a40eca0f601981
Hector_Cardona <hector.cardona@globant.com>
2021-01-26 11:39:37 -0500

    use Runner

--- Data/Calculator.java
+++ Data/Calculator.java
@@ -1,16 +1,16 @@
 package Data;
 
 public class Calculator{
-    public int sum(int number1, int number2){
+    public static int sum(int number1, int number2){
         return number1+number2;
     }
-    public int subtract(int number1, int number2){
+    public static int subtract(int number1, int number2){
         return number1-number2;
     }
-    public int multi(int number1, int number2){
+    public static int multi(int number1, int number2){
         return number1*number2;
     }
-    public int div(int number1, int number2){
+    public static int div(int number1, int number2){
         return number1/number2;
     }
 }
--- Runner/Runner.java
+++ Runner/Runner.java
@@ -1,7 +1,12 @@
 package Runner;
 
+import Data.Calculator;
+
 public class Runner{
     public static void main(String[] args) {
-        
+        System.out.println(Calculator.sum(10,5));
+        System.out.println(Calculator.subtract(10,5));
+        System.out.println(Calculator.multi(10,5));
+        System.out.println(Calculator.div(10,5));
     }
 }
